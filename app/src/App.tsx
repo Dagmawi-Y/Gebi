977046dcfcd810210b8bbe9b3baa490bc62dd2e5
kalkidan360g <kalkidan@360ground.com>
2022-06-25 18:03:25 +0300

    splash screen and app name changed

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StateContextProvider} from './global/context';
 
 import RouteApp from './navigation/MainRouter';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
-
+  useEffect(() => {
+    setTimeout(() => SplashScreen.hide(), 1500);
+  });
   return (
     <StateContextProvider>
       <RouteApp />
